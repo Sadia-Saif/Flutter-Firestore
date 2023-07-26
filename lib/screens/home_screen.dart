1117822af9911a917bd786a0b8f272ba0f2288dc
ahmadexe <muahmad710@gmail.com>
2023-07-26 14:19:52 +0500

    real time data changes reflected on UI using streams

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crud/services/database/users/data_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,7 +9,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -16,6 +16,36 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: StreamBuilder(
+        stream: UserProvider.fetchUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            final users = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index].data();
+                return ListTile(
+                  title: Text(user['name']),
+                  subtitle: Text(user['likes']),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text('No data found.'),
+            );
+          }
+        },
+      ),
+    );
   }
 }
--- lib/services/database/users/data_provider.dart
+++ lib/services/database/users/data_provider.dart
@@ -1,9 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crud/static/constants.dart';
+import 'package:flutter/material.dart';
 
 class UserProvider {
   static final _firestore = FirebaseFirestore.instance;
-
+  static const userCollection = AppConstants.usersCollection;
+  
   static Future<List<Map<String, dynamic>>> getData() async {
-    
+    try { 
+      final users = await _firestore.collection(userCollection).get();
+      return users.docs.map((user) => user.data()).toList();
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    } 
+  }
+
+  static Stream<QuerySnapshot<Map<String, dynamic>>> fetchUser() {
+    try {
+      return _firestore.collection(userCollection).snapshots().asBroadcastStream();
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
   }
 }
