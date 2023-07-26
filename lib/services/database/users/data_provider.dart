53ba64b1d479ffbd5950aa0e640bfac75f898b35
ahmadexe <muahmad710@gmail.com>
2023-07-26 14:27:39 +0500

    used timebased ids

--- lib/services/database/users/data_provider.dart
+++ lib/services/database/users/data_provider.dart
@@ -5,22 +5,42 @@ import 'package:flutter/material.dart';
 class UserProvider {
   static final _firestore = FirebaseFirestore.instance;
   static const userCollection = AppConstants.usersCollection;
-  
+
   static Future<List<Map<String, dynamic>>> getData() async {
-    try { 
+    try {
       final users = await _firestore.collection(userCollection).get();
       return users.docs.map((user) => user.data()).toList();
     } catch (e) {
       debugPrint(e.toString());
       rethrow;
-    } 
+    }
   }
 
   static Stream<QuerySnapshot<Map<String, dynamic>>> fetchUser() {
     try {
-      return _firestore.collection(userCollection).snapshots().asBroadcastStream();
+      return _firestore
+          .collection(userCollection)
+          .snapshots()
+          .asBroadcastStream();
     } catch (e) {
       debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  static Future<void> createUser(String name, String likes, int age) async {
+    try {
+      final payload = {
+        'name': name,
+        'likes': likes,
+        'age': age,
+      };
+
+      await _firestore
+          .collection(userCollection)
+          .doc(DateTime.now().microsecondsSinceEpoch.toString())
+          .set(payload);
+    } catch (e) {
       rethrow;
     }
   }
